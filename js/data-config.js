f62766d4d079cccabaa4c7417abf5c8ed54bc30a
ldc <ducheng.lu@obspm.fr>
2026-07-07 19:41:08 +0000

    chore: update data-config.js with repository info

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'PLACEHOLDER_REPO_OWNER',
+    repoOwner: 'luducheng',
 
     /**
      * GitHub repository name
